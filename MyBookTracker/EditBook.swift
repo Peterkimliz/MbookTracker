11c8d7f715dd7f6f9344dcd6872c1d5f08bceac8
Peterkimliz <p.kironji@yahoo.com>
2026-05-20 05:46:08 -0700

    edit page code

---
+++ MyBookTracker/EditBook.swift
@@ -0,0 +1,18 @@
+//
+//  EditBook.swift
+//  MyBookTracker
+//
+//  Created by Apple on 20/05/2026.
+//
+
+import SwiftUI
+
+struct EditBook: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBook()
+}
