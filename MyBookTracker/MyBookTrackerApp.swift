f5b2c237f1c0bb2a99918e0bf1a12c14babaf29a
Peterkimliz <p.kironji@yahoo.com>
2026-05-20 01:50:03 -0700

    Initial Commit

---
+++ MyBookTracker/MyBookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookTrackerApp.swift
+//  MyBookTracker
+//
+//  Created by Apple on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyBookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
