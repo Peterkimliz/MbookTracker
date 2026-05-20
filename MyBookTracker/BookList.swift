cb773e4bb613c0266a679e96f4250cb956d3f5f8
Peterkimliz <p.kironji@yahoo.com>
2026-05-20 04:02:41 -0700

    added the listing of the books in the books list page

--- MyBookTracker/BookList.swift
+++ MyBookTracker/BookList.swift
@@ -6,21 +6,100 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct BookList: View {
+    @Query(sort:\Book.title) private var books:[Book]
+    @State private var showSheet:Bool=false
+    @Environment(\.modelContext) private var context
+    
+    
     var body: some View {
         NavigationStack{
             
-            
-            VStack{
+            Group{
+                
+                if(books.isEmpty){
+                    ContentUnavailableView("No books found", systemImage: "book.fill")
+                    
+                }
+                else{
+                    List{
+                        ForEach(books) { book in
+                            NavigationLink {
+                                Text(book.title)
+                            } label: {
+                                HStack{
+                                    book.icon
+                                    
+                                    VStack(alignment: .leading){
+                                        Text(book.title)
+                                            .font(.title2)
+                                        Text(book.author)
+                                            .foregroundStyle(.secondary)
+                                        
+                                        if let rating=book.rating{
+                                            HStack{
+                                                ForEach(1..<rating,id:\.self){inde in
+                                                    Image(systemName: "star.fill")
+                                                        .imageScale(.small)
+                                                        .foregroundStyle(.yellow)
+                                                    
+                                                }
+                                                
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                }
+                            }
+                            
+                        }.onDelete { IndexSet in
+                            IndexSet.forEach { index in
+                                let book=books[index]
+                                context.delete(book)
+                                do{
+                                    try context.save()
+                                }catch{
+                                    print("Error occurred is\(error)")
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                
+                
                 
                 
             }
-        }.navigationTitle("My Books")
+            .listStyle(.plain)
+            .navigationTitle("My Books")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button {
+                        showSheet=true
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .imageScale(.large)
+                    }
+                    
+                }
+            }.sheet(isPresented: $showSheet) {
+                NewBook()
+                    .presentationDetents([.medium])
+                    .presentationDragIndicator(.visible)
+                
+            }
+            
+            
+            
+        }
         
-    
-    }
-}
+        
+    }}
+
 
 #Preview {
     BookList()
--- MyBookTracker/MyBookTrackerApp.swift
+++ MyBookTracker/MyBookTrackerApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MyBookTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             BookList()
-        }
+        }.modelContainer(for: Book.self)
+    }
+    
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
